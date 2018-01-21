5d2add471c1f9a5397e761ba668e50092899a4ec
shompot <cholpon94@gmail.com>
2018-01-21 19:02:16 +0300

    logo is done, some other modificaions done
    need to do gallery, finish contact

---
+++ style/js/gallery.js
@@ -0,0 +1,63 @@
+jQuery(document).ready(function($) {
+		// We only want these styles applied when javascript is enabled
+		$('div.navigation').css({'width' : '960px', 'float' : 'left'});
+		$('div.content').css('display', 'block');
+
+		// Initially set opacity on thumbs and add
+		// additional styling for hover effect on thumbs
+		var onMouseOutOpacity = 0.67;
+		$("#thumbs ul.thumbs li").css("opacity", onMouseOutOpacity);
+		$("#thumbs ul.thumbs li").hover(function() {
+			 $(this).animate({opacity: 1.0}, 500);
+		}, function() {
+			 $(this).animate({opacity: onMouseOutOpacity}, 500);
+		});
+
+		// Initialize Advanced Galleriffic Gallery
+		var gallery = $('#thumbs').galleriffic({
+			delay:                     2500,
+			numThumbs:                 5,
+			preloadAhead:              10,
+			enableTopPager:            false,
+			enableBottomPager:         false,
+			maxPagesToShow:            7,
+			imageContainerSel:         '#slideshow',
+			controlsContainerSel:      '#controls',
+			captionContainerSel:       '#caption',
+			loadingContainerSel:       '#loading',
+			renderSSControls:          true,
+			renderNavControls:         true,
+			playLinkText:              'Play Slideshow',
+			pauseLinkText:             'Pause Slideshow',
+			prevLinkText:              '&lsaquo; Previous Photo',
+			nextLinkText:              'Next Photo &rsaquo;',
+			nextPageLinkText:          'Next &rsaquo;',
+			prevPageLinkText:          '&lsaquo; Prev',
+			enableHistory:             false,
+			autoStart:                 true,
+			syncTransitions:           true,
+			defaultTransitionDuration: 900,
+			onSlideChange:             function(prevIndex, nextIndex) {
+				// 'this' refers to the gallery, which is an extension of $('#thumbs')
+				this.find('ul.thumbs').children()
+					.eq(prevIndex).fadeTo('fast', onMouseOutOpacity).end()
+					.eq(nextIndex).fadeTo('fast', 1.0);
+			},
+			onPageTransitionOut:       function(callback) {
+				this.fadeTo('fast', 0.0, callback);
+			},
+			onPageTransitionIn:        function() {
+				this.fadeTo('fast', 1.0);
+			}
+		});
+			gallery.find('a.prev').click(function(e) {
+			gallery.previousPage();
+			e.preventDefault();
+		});
+
+		gallery.find('a.next').click(function(e) {
+			gallery.nextPage();
+			e.preventDefault();
+		});
+
+});
